e4f620e750bcfe400891189615740175a504b931
William Davies <wd6g14@soton.ac.uk>
2017-05-04 16:49:04 +0100

    Basic player movement and camera

---
+++ Game-Dev-CW3/Assets/PlayerMovementController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+
+    public float MoveSpeed = 10f;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal") * MoveSpeed;
+        var vertical = Input.GetAxis("Vertical") * MoveSpeed;
+        var currentPos = transform.position;
+        transform.position = Vector3.Lerp(currentPos, new Vector3(currentPos.x + horizontal, currentPos.y + vertical, currentPos.z), Time.deltaTime);
+    }
+}
