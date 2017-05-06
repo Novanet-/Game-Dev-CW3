b9264d2ea01cb86fa0e772998fa4b0a72dd0230f
William Davies <wd6g14@soton.ac.uk>
2017-05-06 17:08:35 +0100

    Work on firing and aiming

--- Assets/Scripts/BackgroundRow.cs
+++ Assets/Scripts/BackgroundRow.cs
@@ -1,56 +1,57 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class BackgroundRow : MonoBehaviour {
+namespace Assets.Scripts
+{
+    public class BackgroundRow : MonoBehaviour {
 
-    public GameObject floorTile;
-    public GameObject holeTile;
-    public GameObject wallTile;
-    public GameObject powerup;
+        public GameObject floorTile;
+        public GameObject holeTile;
+        public GameObject wallTile;
+        public GameObject powerup;
 
-    public int numTiles = 11;
+        public int numTiles = 11;
 
-    public float tileWidth = 0.75f;
+        public float tileWidth = 0.75f;
 
-    public int maxObsticles = 5;
+        public int maxObsticles = 5;
 
-    // Use this for initialization
-    void Start () {
-        
-	}
+        // Use this for initialization
+        private void Start () {
 
-    public void Create(float floorPercent, float holePercent, float wallPercent) {
-        float pos = -(tileWidth * Mathf.Floor(numTiles / 2));
-
-        int numObsticles = 0;
-
-        for (int i = 0; i < numTiles; i++) {
-            float n = Random.Range(0, 1f);
-
-            if (n < floorPercent || numObsticles >= maxObsticles) {
-                GameObject tile = GameObject.Instantiate(floorTile, this.transform);
-                tile.transform.position = new Vector3(pos, 0, 0);
-            }
-            else if (n < floorPercent + holePercent) {
-                GameObject tile = GameObject.Instantiate(holeTile, this.transform);
-                tile.transform.position = new Vector3(pos, 0, 0);
-                numObsticles++;
-            }
-            else if (n < floorPercent + holePercent + wallPercent) {
-                GameObject tile = GameObject.Instantiate(wallTile, this.transform);
-                tile.transform.position = new Vector3(pos, 0, 0);
-                numObsticles++;
-            }
-
-            pos += tileWidth;
         }
 
+        public void Create(float floorPercent, float holePercent, float wallPercent) {
+            float pos = -(tileWidth * Mathf.Floor(numTiles / 2));
 
+            int numObsticles = 0;
+
+            for (int i = 0; i < numTiles; i++) {
+                float n = Random.Range(0, 1f);
+
+                if (n < floorPercent || numObsticles >= maxObsticles) {
+                    GameObject tile = GameObject.Instantiate(floorTile, this.transform);
+                    tile.transform.position = new Vector3(pos, 0, 0);
+                }
+                else if (n < floorPercent + holePercent) {
+                    GameObject tile = GameObject.Instantiate(holeTile, this.transform);
+                    tile.transform.position = new Vector3(pos, 0, 0);
+                    numObsticles++;
+                }
+                else if (n < floorPercent + holePercent + wallPercent) {
+                    GameObject tile = GameObject.Instantiate(wallTile, this.transform);
+                    tile.transform.position = new Vector3(pos, 0, 0);
+                    numObsticles++;
+                }
+
+                pos += tileWidth;
+            }
+
+
+        }
+
+        // Update is called once per frame
+        private void Update () {
+
+        }
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -1,56 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerMovementController : MonoBehaviour
+namespace Assets.Scripts
 {
-    public float MoveSpeed = 10f;
-    public float TurnSpeed = 10f;
-    public float RotationOffset = -90f;
-    // Use this for initialization
-    void Start()
+    public class PlayerMovementController : MonoBehaviour
     {
-    }
+        public float MoveSpeed = 10f;
+        public float TurnSpeed = 10f;
+        public float RotationOffset = -90f;
+        // Use this for initialization
+        private void Start()
+        {
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-//        var horizontal = Input.GetAxis("Horizontal") * MoveSpeed;
-//        var vertical = Input.GetAxis("Vertical") * MoveSpeed;
-//        var currentPos = transform.position;
-//        transform.position = Vector3.Lerp(currentPos, new Vector3(currentPos.x + horizontal, currentPos.y + vertical, currentPos.z), Time.deltaTime);
+        // Update is called once per frame
+        private void Update()
+        {
+            Controls();
+            HelperFunctions.ClampTransformToCameraView(transform);
+        }
 
-        Controls();
-    }
-
-    void Controls()
-    {
-        var horizontal = Input.GetAxis("Horizontal") * MoveSpeed;
-        var vertical = Input.GetAxis("Vertical") * MoveSpeed;
+        private void Controls()
+        {
+            var horizontal = Input.GetAxis("Horizontal") * MoveSpeed;
+            var vertical = Input.GetAxis("Vertical") * MoveSpeed;
 
 
-        var currentPos = transform.position;
-        var newPos = new Vector3(currentPos.x + horizontal, currentPos.y + vertical, currentPos.z);
-        transform.position = Vector3.Lerp(currentPos, newPos, Time.deltaTime);
+            var currentPos = transform.position;
+            var newPos = new Vector3(currentPos.x + horizontal, currentPos.y + vertical, currentPos.z);
+            transform.position = Vector3.Lerp(currentPos, newPos, Time.deltaTime);
 
-        //        transform.position = Vector3.Lerp(currentPos, newPos, Time.deltaTime);
-        //        transform.position += moveDirection * MoveSpeed * Time.deltaTime;
+            if (Mathf.Abs(horizontal) <= 0 && Mathf.Abs(vertical) <= 0) return;
 
-        if (Mathf.Abs(horizontal) <= 0 && Mathf.Abs(vertical) <= 0) return;
-        //
-        //        float angle = Mathf.Atan2(horizontal * 1 * -1, vertical * 1) * Mathf.Rad2Deg;
-        //        var currentRotation = transform.rotation;
-        //        var newRotation = Quaternion.Euler(new Vector3(0, 0, angle));
-        //        transform.rotation = Quaternion.Slerp(currentRotation, newRotation, Time.deltaTime);
 
-        Vector3 moveDirection = newPos - currentPos;
-        moveDirection.z = 0;
-        moveDirection.Normalize();
+            Vector3 moveDirection = newPos - currentPos;
+            moveDirection.z = 0;
+            moveDirection.Normalize();
 
-        float targetAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            float targetAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
 
-        Quaternion currentRotation = transform.rotation;
-        Quaternion newRotation = Quaternion.Euler(0, 0, targetAngle + RotationOffset);
-        transform.rotation = Quaternion.Slerp(currentRotation, newRotation, Time.deltaTime * TurnSpeed);
+            Quaternion currentRotation = transform.rotation;
+            Quaternion newRotation = Quaternion.Euler(0, 0, targetAngle + RotationOffset);
+            transform.rotation = Quaternion.Slerp(currentRotation, newRotation, Time.deltaTime * TurnSpeed);
+        }
     }
 }
