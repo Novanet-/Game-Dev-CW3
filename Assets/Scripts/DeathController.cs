28281541d18ff859095607c96cad6ac534cb7a66
William Davies <wd6g14@soton.ac.uk>
2017-05-07 17:53:16 +0100

    Fixed git fuckup hopefully

--- Assets/Scripts/DeathController.cs
+++ Assets/Scripts/DeathController.cs
@@ -2,50 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeathController : MonoBehaviour {
+public class DeathController : MonoBehaviour
+{
     public bool canFall = false;
 
-    public void Die() {
+    public void Die()
+    {
         Destroy(this.gameObject);
     }
 
-    public void Die(GameObject self) {
+    public void Die(GameObject self)
+    {
         Destroy(self);
     }
 
-    public void OnTriggerEnter2D(Collider2D coll) {
+    public void OnTriggerEnter2D(Collider2D coll)
+    {
         print(canFall + " " + coll.tag);
-        if (coll.tag == "Bullet") {
+        if (coll.CompareTag("Bullet"))
+        {
             Die();
-        }
-
-        public void Die(GameObject self) {
-            Destroy(self);
         }
     }
 }
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DeathController : MonoBehaviour {
-    public bool canFall = false;
-    public bool isPlayer = false;
-
-    public void Die() {
-        Destroy(this.gameObject);
-    }
-
-    public void Die(GameObject self) {
-        Destroy(self);
-    }
-
-    public void OnTriggerEnter2D(Collider2D coll) {
-        print(isPlayer + " " + coll.tag);
-        if (coll.tag == "Bullet") {
-            if ((coll.GetComponent<EnemyLaserController>() && isPlayer) || (coll.GetComponent<LaserController>() && !isPlayer))
-                Die();
-        else if (isPlayer && coll.tag == "Enemy") {
-            Die(GameObject.Find("Player"));
-        }
-    }
