233e7bd13f63e1a0e73cf55b23e47edf8867e7b5
William Davies <wd6g14@soton.ac.uk>
2017-05-08 21:29:16 +0100

    Merge fixes

--- Assets/Scripts/DeathController.cs
+++ Assets/Scripts/DeathController.cs
@@ -54,42 +54,4 @@ namespace Assets.Scripts {
     }
 }
 
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-namespace Assets.Scripts {
-    public class DeathController : MonoBehaviour {
-        public bool canFall = false;
-        public bool isPlayer = false;
-
-        public void Die() {
-            Destroy(this.gameObject);
-        }
-
-        public void Die(GameObject self) {
-            Destroy(self);
-        }
-
-        public void OnTriggerEnter2D(Collider2D coll) {
-            if (coll.tag == "Bullet") {
-                if ((coll.GetComponent<EnemyLaserController>() && isPlayer) || (coll.GetComponent<LaserController>() && !isPlayer)) {
-                    if (isPlayer)
-                        Die();
-                    else {
-                        Die(GetComponentInParent<EnemyController>().gameObject);
-                    }
-
-                    Die(coll.gameObject);
-                }
-            }
-            else if (canFall && coll.tag == "Hole") {
-                Die(GameObject.Find("Player"));
-            }
-            else if (isPlayer && coll.tag == "Enemy") {
-                Die(GameObject.Find("Player"));
-                Die(coll.GetComponentInParent<EnemyController>().gameObject);
-            }
-        }
-    }
-}
--- Assets/Scripts/EnemyFireController.cs
+++ Assets/Scripts/EnemyFireController.cs
@@ -43,36 +43,3 @@ namespace Assets.Scripts {
         }
     }
 }
-
-    private float CurrentTime { get; set; }
-    private float NextFireSlot { get; set; }
-
-    private void Start() {
-        NextFireSlot = 0.5f;
-        LaserContainer = GameObject.Find("LaserContainer");
-        AimTarget = GameObject.Find("Player");
-    }
-
-    private void Update() {
-        CurrentTime += Time.deltaTime;
-
-        var position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        if (CurrentTime <= NextFireSlot) return;
-
-        NextFireSlot = CurrentTime + FireInterval;
-
-        if (AimTarget != null)
-        {
-            GameObject bullet = Instantiate(LaserType, transform.position, transform.rotation, transform);
-
-            if (transform.rotation.eulerAngles.y > 90)
-            {
-                bullet.transform.Rotate(new Vector3(0, 180, 0));
-            }
-        }
-
-        NextFireSlot -= CurrentTime;
-        CurrentTime = 0.0F;
-    }
-}
