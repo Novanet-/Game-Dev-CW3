d68dbd6b19a56498742487b1db8e73c41a709286
Matt Andrew <meja1g14@soton.ac.uk>
2017-05-09 17:21:25 +0100

    Added suppressor and coloured enemies

---
+++ Assets/Scripts/Entity/EnemyControllerSuppress.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Entity {
+    public class EnemyControllerSuppress : EnemyController {
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Entity/EnemyFireControllerUnaimed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Entity {
+    public class EnemyFireControllerUnaimed : EnemyFireController {
+        void Update() {
+            CurrentTime += Time.deltaTime;
+
+            Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (CurrentTime <= NextFireSlot) return;
+
+            NextFireSlot = CurrentTime + FireInterval;
+
+            if (AimTarget != null && _spriteRenderer.isVisible) {
+                GameObject bullet = Instantiate(LaserType, transform.position, transform.rotation, transform);
+
+                if (transform.rotation.eulerAngles.y > 90) {
+                    bullet.transform.Rotate(new Vector3(0, 180, 0));
+                }
+            }
+
+            NextFireSlot -= CurrentTime;
+            CurrentTime = 0.0F;
+        }
+    }
+}
