a32e2e91f2c973321b8d582c365dab0e2e982362
William Davies <wd6g14@soton.ac.uk>
2017-05-11 14:02:31 +0100

    Interactive startscreen

--- Assets/Scripts/UI/StartUIController.cs
+++ Assets/Scripts/UI/StartUIController.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using com.kleberswf.lib.core;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace UI
 {
     public class StartUIController : Singleton<StartUIController>
     {
+        public void PlayGame()
+        {
+            Debug.Log("Play");
+            SceneManager.LoadSceneAsync("MainScene");
+        }
 
+        public void QuitGame()
+        {
+            Debug.Log("Quit");
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
     }
 }
