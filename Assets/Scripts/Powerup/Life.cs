6ad4f45e166c6914f0e0915208a4d94fbb7c3345
Matt Andrew <meja1g14@soton.ac.uk>
2017-05-16 18:05:44 +0100

    Added life spawner

---
+++ Assets/Scripts/Powerup/Life.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Powerup {
+    public class Life : MonoBehaviour {
+
+        #region Private Fields
+
+        private Entity.PlayerDeathController _player;
+
+        #endregion Private Fields
+
+        #region Public Methods
+
+        // Use this for initialization
+        void Start() {
+            _player = GameObject.Find("Player").GetComponent<Entity.PlayerDeathController>();
+        }
+
+        public void OnTriggerEnter2D(Collider2D coll) {
+            if (coll.tag == "Player") {
+                _player.AddLife();
+
+                Destroy(this.gameObject);
+            }
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ Assets/Scripts/Powerup/LifeSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Powerup {
+    public class LifeSpawner : MonoBehaviour {
+        #region Public Fields
+
+        public Life powerup;
+        public int timeDelay = 20;
+
+        #endregion Public Fields
+
+        #region Private Fields
+
+        private float time;
+
+        #endregion Private Fields
+
+        #region Private Methods
+
+        // Use this for initialization
+        void Start() {
+            time = Time.time + timeDelay + 5;
+        }
+
+        // Update is called once per frame
+        void Update() {
+            if (time < Time.time) {
+                time += timeDelay;
+
+                Instantiate(powerup);
+            }
+        }
+
+        #endregion Private Methods
+    }
+}
